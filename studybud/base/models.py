f62870ce8f3eedc0ec904af7033d4c8fad798ff9
Zu18 <zuhra.galieva.2014@gmail.com>
2022-12-02 14:47:31 +0000

    Create model Topic

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
-    #host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    #topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants = models.ManyToManyField(
@@ -22,5 +29,5 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-        def __str__(self):
-            return self.body[0:50]
+    def __str__(self):
+        return self.body[0:50]
